8bbbc87766d783c1cbe5b21c5b1581f6f1e2b1e3
MazayKun <kir99916@gmail.com>
2023-07-14 20:22:08 +0300

    Провел небольшое исследование работы с хипом

--- gc-homework/src/main/java/ru/calculator/Summator.java
+++ gc-homework/src/main/java/ru/calculator/Summator.java
@@ -4,18 +4,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Summator {
-    private Integer sum = 0;
-    private Integer prevValue = 0;
-    private Integer prevPrevValue = 0;
-    private Integer sumLastThreeValues = 0;
-    private Integer someValue = 0;
-    private final List<Data> listValues = new ArrayList<>();
+    private int sum = 0;
+    private int prevValue = 0;
+    private int prevPrevValue = 0;
+    private int sumLastThreeValues = 0;
+    private int someValue = 0;
+    private int counter = 0;
 
     //!!! сигнатуру метода менять нельзя
     public void calc(Data data) {
-        listValues.add(data);
-        if (listValues.size() % 6_600_000 == 0) {
-            listValues.clear();
+       counter++;
+        if (counter % 6_600_000 == 0) {
+            counter = 0;
         }
         sum += data.getValue();
 
@@ -24,9 +24,9 @@ public class Summator {
         prevPrevValue = prevValue;
         prevValue = data.getValue();
 
-        for (var idx = 0; idx < 3; idx++) {
+        for (int idx = 0; idx < 3; idx++) {
             someValue += (sumLastThreeValues * sumLastThreeValues / (data.getValue() + 1) - sum);
-            someValue = Math.abs(someValue) + listValues.size();
+            someValue = Math.abs(someValue) + counter;
         }
     }
 
